e9b2358b69a3d1a0cb8e86dbf0f6c61899f3844b
AlexMega <matvienko.alex1205@gmail.com>
2024-09-22 14:47:54 +0300

    LR2.2

--- ASPNET.lr1/Program.cs
+++ ASPNET.lr1/Program.cs
@@ -1,45 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .AddXmlFile("companies.xml", optional: true, reloadOnChange: true)
-    .AddIniFile("companies.ini", optional: true, reloadOnChange: true);
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
 var app = builder.Build();
 
-app.MapGet("/", (IConfiguration config) =>
+app.MapGet("/about", (IConfiguration config) =>
 {
-    var companies = new List<Company>();
+    var userInfo = config.GetSection("UserInfo").Get<UserInfo>();
 
-    // JSON-файл
-    companies.AddRange(config.GetSection("Companies").Get<List<Company>>());
-
-    // XML-файл
-    var xmlCompanies = config.GetSection("Companies").GetChildren().Select(x => new Company
-    {
-        Name = x["Name"],
-        Employees = int.Parse(x["Employees"])
-    });
-    companies.AddRange(xmlCompanies);
-
-    // INI-файл
-    var iniCompanies = new List<Company>
-    {
-        new Company { Name = "Microsoft", Employees = int.Parse(config["Microsoft:Employees"]) },
-        new Company { Name = "Apple", Employees = int.Parse(config["Apple:Employees"]) },
-        new Company { Name = "Google", Employees = int.Parse(config["Google:Employees"]) }
-    };
-    companies.AddRange(iniCompanies);
-
-    var topCompany = companies.OrderByDescending(c => c.Employees).FirstOrDefault();
-
-    return $"Компанія з найбільшою кількістю співробітників: {topCompany.Name}, {topCompany.Employees} співробітників";
+    return $"Ім'я: {userInfo.Name}\nВік: {userInfo.Age}\nМісто: {userInfo.City}";
 });
 
 app.Run();
 
-public class Company
+public class UserInfo
 {
     public string Name { get; set; }
-    public int Employees { get; set; }
+    public int Age { get; set; }
+    public string City { get; set; }
 }
